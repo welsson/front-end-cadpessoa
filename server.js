0d5de4396cd73b4ac2967eecf5b14df943525bf5
welsson <welssoncavalcante@gmail.com>
2020-08-17 22:26:26 -0300

    Front end pessoa

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const nameApp = 'cadpessoa-app';
+const app = express();
+// Servir arquivos estáticos
+app.use(express.static(`${__dirname}/dist/${nameApp}`));
+// Envie todos os pedidos para index.html
+app.get('/*', function(req, res) {
+res.sendFile(path.join(`${__dirname}/dist/${nameApp}/index.html`));
+});
+// Porta padrão do Heroku
+app.listen(process.env.PORT || 5000);
